917a2b606c8892d6aff66e06abcc62293c911d62
Ahmed Samara <asamara@ncsu.edu>
2014-04-01 23:12:16 -0400

    Update gps.c

--- gps.c
+++ gps.c
@@ -2,7 +2,14 @@
 #include "macros.h"
 #include "functions.h"
 
-void GpsInit(void)
-{
-  
+void init_gps(void){
+    PJOUT &= ~GPS_PWRCNTL;
+    PJOUT |= GPS_PWRCNTL;
+
+    while(!(PJIN & GPS_PWRCHK)){
+      //Wait for it to transition to high
+    }
+    //Once PWRCHK is high, release GPS_PWRCNTL
+    //By setting to low.
+    PJOUT &= GPS_PWRCNTL;
 }

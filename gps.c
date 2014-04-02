92b3559ebcfffbd11d26934695dc5d1602c9c870
ncsu-nghansen <nghansen@ncsu.edu>
2014-04-01 23:13:14 -0400

    Update gps.c

--- gps.c
+++ gps.c
@@ -6,9 +6,8 @@ void init_gps(void){
     PJOUT &= ~GPS_PWRCNTL;
     PJOUT |= GPS_PWRCNTL;
 
-    while(!(PJIN & GPS_PWRCHK)){
+    while(!(PJIN & GPS_PWRCHK));
       //Wait for it to transition to high
-    }
     //Once PWRCHK is high, release GPS_PWRCNTL
     //By setting to low.
     PJOUT &= GPS_PWRCNTL;

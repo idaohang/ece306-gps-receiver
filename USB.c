e707eb425e1c630d560ca7e6d6b8566ddae0b50f
Ahmed Samara <asamara@ncsu.edu>
2014-04-01 23:01:14 -0400

    added file for functions related to interaction with USB

---
+++ USB.c
@@ -0,0 +1,12 @@
+//Include Statements
+#include  "msp430.h"
+#include  "functions.h"
+#include  "macros.h"
+
+
+//// Port J Macros
+//#define GPS_PWR (0x01)
+//#define GPS_RESET (0x02)
+//#define GPS_PWRCNTL (0x04)
+//#define GPS_PWRCHK (0x08)
+
